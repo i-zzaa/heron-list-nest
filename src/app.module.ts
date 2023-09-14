68140741373f7b44fba18397f67e97ae429a3be9
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-09-14 14:48:39 -0300

    fix: ajuste

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { ServiceService } from './service/service.service';
 import { TicketService } from './ticket/ticket.service';
 import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './user/users.module';
 
 import * as cors from 'cors';
 
--- src/user/users.module.ts
+++ src/user/users.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { UsersService } from './user.service';
 
 import { SoapModule } from 'nestjs-soap';
 
