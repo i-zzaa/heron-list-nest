61d7fb3dceb9a661bf3c791b04ee8804e695ff6c
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-09-14 14:49:13 -0300

    fix: ajuste

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,41 +1,146 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { Client } from 'nestjs-soap';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { DEVICE, DeviceProps, moneyFormat } from 'src/util/util';
+import { UserProps } from './user.interface';
 
 @Injectable()
-export class UsersService {
-  // constructor() {} // private readonly soapService: Client, // @Inject(process.env.SOAP_CLIENT_NAME)
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
 
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'andressa.novaes',
-      password: '12345678',
-    },
-    {
-      userId: 2,
-      username: 'root.localhost',
-      password: '123456',
-    },
-    {
-      userId: 3,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
+  async findUserAuth(username: string): Promise<any | undefined> {
+    const user: UserProps = await this.prismaService.usuario.findFirstOrThrow({
+      select: {
+        id: true,
+        nome: true,
+        login: true,
+        senha: true,
+        perfil: true,
+        ativo: true,
+        permissoes: {
+          select: {
+            permissao: {
+              select: {
+                cod: true,
+              },
+            },
+          },
+        },
+      },
+      where: {
+        login: username,
+      },
+    });
 
-  async findOne(username: string, password: string): Promise<any | undefined> {
-    return this.users.find((user: any) => user.username === username);
-    // return new Promise((resolve, reject) => {
-    //   this.soapService.TicketConnector(
-    //     { CustomerUserLogin: username, Password: password },
-    //     (err: any, res: any) => {
-    //       if (res) {
-    //         resolve(res);
-    //       } else {
-    //         reject(err);
-    //       }
-    //     },
-    //   );
-    // });
+    return user;
+  }
+
+  async getUsers(page: number, pageSize: number): Promise<any | undefined> {
+    const skip = (page - 1) * pageSize;
+
+    const [usuarios, totalItems] = await Promise.all([
+      this.prismaService.usuario.findMany({
+        select: {
+          id: true,
+          nome: true,
+          login: true,
+          perfil: true,
+          ativo: true,
+          permissoes: {
+            include: {
+              permissao: true,
+            },
+          },
+          terapeuta: {
+            include: {
+              especialidade: {
+                select: {
+                  nome: true,
+                  id: true,
+                },
+              },
+              funcoes: {
+                include: {
+                  funcao: true,
+                },
+              },
+            },
+          },
+        },
+        orderBy: {
+          nome: 'asc',
+        },
+        where: {
+          ativo: true,
+          NOT: {
+            perfil: {
+              nome: {
+                in: ['developer', 'Developer'],
+              },
+            },
+          },
+        },
+        skip,
+        take: pageSize,
+      }),
+      this.prismaService.usuario.count(),
+    ]);
+
+    const totalPages = Math.ceil(totalItems / pageSize); // Calcula o total de páginas
+
+    const data = await Promise.all(
+      usuarios.map((usuario: any) => {
+        const funcoesId = usuario?.terapeuta?.funcoes.map((funcao: any) => {
+          return {
+            nome: funcao.funcao.nome,
+            id: funcao.funcao.id,
+          };
+        });
+
+        const permissoesId = usuario?.permissoes.map(
+          ({ permissao }: any) => permissao.id,
+        );
+
+        delete usuario.permissoes;
+
+        if (usuario?.terapeuta?.fazDevolutiva) {
+          usuario.devolutiva = usuario?.terapeuta?.fazDevolutiva;
+        }
+
+        if (usuario?.terapeuta?.cargaHoraria) {
+          usuario.cargaHoraria = JSON.parse(usuario.terapeuta?.cargaHoraria);
+        }
+
+        if (usuario?.terapeuta?.funcoes) {
+          usuario.comissao = usuario?.terapeuta?.funcoes.map((funcao: any) => {
+            const comissao =
+              funcao.tipo === 'Fixo'
+                ? moneyFormat.format(parseFloat(funcao.comissao))
+                : funcao.comissao;
+
+            return {
+              funcaoId: funcao.funcaoId,
+              valor: comissao,
+              tipo: funcao.tipo,
+              funcao: funcao.funcao.nome,
+            };
+          });
+        }
+
+        return {
+          ...usuario,
+          especialidadeId: usuario?.terapeuta?.especialidade,
+          permissoesId: permissoesId,
+          funcoesId: funcoesId,
+        };
+      }),
+    );
+
+    const pagination = {
+      currentPage: page,
+      pageSize,
+      totalPages,
+    };
+
+    return { data, pagination };
   }
 }
