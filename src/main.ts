8e08d971da0c0dc591325512eb26cbfb2a4618b7
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-09-19 16:37:26 -0300

    feat: implementa chatbot envio de mensagem a partir dos eventos

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cron from 'node-cron';
-import { VenomBotAdapter } from 'src/whatsApp/whatsApp.adapter';
 import { WhatsappService } from 'src/whatsApp/whatsApp.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  const venomBotAdapter = app.get(VenomBotAdapter);
-  await venomBotAdapter.start();
-
   const whatsappService = app.get(WhatsappService);
+  await whatsappService.start();
 
   // Configuração da tarefa agendada para executar todos os dias às 12h
   cron.schedule('0 12 * * *', () => {
--- src/whatsApp/whatsApp.module.ts
+++ src/whatsApp/whatsApp.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { WhatsappService } from './whatsApp.service';
+import { WhatsappService } from 'src/whatsApp/whatsApp.service';
 import { AgendaService } from 'src/agenda/agenda.service';
 import { BaixaModule } from 'src/baixa/baixa.module';
 import { FrequenciaModule } from 'src/frequencia/frequencia.module';
@@ -8,11 +8,10 @@ import { PacienteModule } from 'src/paciente/paciente.module';
 import { UserModule } from 'src/user/user.module';
 import { VagaModule } from 'src/vaga/vaga.module';
 import { WhatsappController } from './whatsApp.controller';
-import { VenomBotAdapter } from './whatsApp.adapter';
 import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [WhatsappService, PrismaService, VenomBotAdapter, AgendaService],
+  providers: [WhatsappService, PrismaService, AgendaService],
   exports: [WhatsappService],
   controllers: [WhatsappController],
   imports: [
