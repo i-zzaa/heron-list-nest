ee618fb6aaf0383eb2122b4db00ef692ede92637
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-09-19 16:37:26 -0300

    feat: implementa chatbot envio de mensagem a partir dos eventos

--- src/whatsApp/whatsApp.service.ts
+++ src/whatsApp/whatsApp.service.ts
@@ -1,14 +1,116 @@
 import { Injectable } from '@nestjs/common';
-import { VenomBotAdapter } from './whatsApp.adapter';
 import { AgendaService } from 'src/agenda/agenda.service';
 import { formatDateHours, getDateBeforeDay } from 'src/util/format-date';
-
+import { create, Whatsapp } from 'venom-bot';
+import * as fs from 'fs';
 @Injectable()
 export class WhatsappService {
-  constructor(
-    private readonly venomBotAdapter: VenomBotAdapter,
-    private readonly agendaService: AgendaService,
-  ) {}
+  private client: Whatsapp;
+  private sessionPath: string = 'session.json';
+  private logPath: string = 'log-mwssage.json';
+
+  constructor(private readonly agendaService: AgendaService) {}
+
+  async start() {
+    // Verifica se há uma sessão salva
+    const sessionData = this.loadSessionData();
+
+    // Inicializa o cliente Venom com a sessão salva ou sem gerar QR code
+    this.client = await create(
+      'multialcance',
+      (base64Qr, asciiQR) => {
+        // Handle o QR code aqui, se necessário
+        console.log('QR code:', asciiQR);
+      },
+      (statusFind) => {
+        console.log(statusFind);
+      },
+      {
+        disableWelcome: true,
+        updatesLog: false,
+        session: sessionData, // Atribui a sessão carregada, se existir
+      },
+    );
+
+    // Se houver sessão salva, não será necessário salvar novamente
+    if (!sessionData) {
+      // Salva a sessão ao autenticar
+      this.client.onStateChange(async (state) => {
+        if (state === 'CONFLICT' || state === 'UNPAIRED') {
+          // Limpe o arquivo da sessão, pois está inválida
+          fs.unlinkSync(this.sessionPath);
+        } else if (state === 'CONNECTED') {
+          // Salva a sessão
+          const session = await this.client.session;
+          fs.writeFileSync(this.sessionPath, JSON.stringify(session));
+        }
+      });
+    }
+
+    // Lidere com as mensagens recebidas
+    this.client.onMessage(async (message: any) => {
+      // Lide com a mensagem recebida aqui
+      if (!message.isGroupMsg) {
+        switch (message.text) {
+          case '1':
+            break;
+          default:
+            var menu =
+              '👋 Olá, como vai? \n\nEm breve estaremos te atendendo. 🥰';
+            this.client
+              .sendText(message.from, menu)
+              .then(() => {})
+              .catch((error) => {
+                fs.writeFileSync(
+                  this.logPath,
+                  JSON.stringify({
+                    data: new Date(),
+                    message: message.text,
+                    from: message.from,
+                  }),
+                );
+              });
+
+            break;
+        }
+      }
+    });
+  }
+
+  private loadSessionData(): any {
+    if (fs.existsSync(this.sessionPath)) {
+      const sessionJson = fs.readFileSync(this.sessionPath, 'utf-8');
+      return JSON.parse(sessionJson);
+    }
+    return null;
+  }
+
+  async sendMessageWhatsapp(to: string, title: string, message: string) {
+    if (!this.client) {
+      throw new Error('Cliente não inicializado.');
+    }
+    const buttons: any = [
+      {
+        buttonText: {
+          displayText: 'CONFIRMAR',
+        },
+      },
+      {
+        buttonText: {
+          displayText: 'CANCELAR',
+        },
+      },
+    ];
+
+    await this.client
+      .sendButtons(to, title, buttons, message)
+      .then((result) => {
+        console.log('Result: ', result); //return object success
+      })
+      .catch((erro) => {
+        console.error('Error when sending: ', erro); //return object error
+      });
+  }
 
   /*
     Chama os eventos e  dispara as mensagens
@@ -51,10 +153,6 @@ export class WhatsappService {
 
     const to = `55${phoneNumber}@c.us`; // Substitua pelo número real do destinatário
 
-    await this.venomBotAdapter.sendMessage(
-      to,
-      `Boa tarde, ${responsavel}!`,
-      message,
-    );
+    await this.sendMessageWhatsapp(to, `Boa tarde, ${responsavel}!`, message);
   }
 }
