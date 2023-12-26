9ce4a4e8d6c62f05b1dc84df98a4f92340b81a59
c1321644 Isa Andressa Novaes Menezes <c1321644@interno.bb.com.br>
2023-12-26 16:29:48 -0300

    fix: ajustes

---
+++ src/programa/programa.controller.ts
@@ -0,0 +1,78 @@
+import {
+  Controller,
+  UseGuards,
+  Get,
+  Post,
+  Body,
+  Put,
+  Delete,
+  Param,
+  Response,
+  Request,
+} from '@nestjs/common';
+
+import { ProgramaProps } from './programa.interface';
+import { responseError, responseSuccess } from 'src/util/response';
+import { AuthGuard } from '@nestjs/passport';
+import { ProgramaService } from './programa.service';
+
+@UseGuards(AuthGuard('jwt'))
+@Controller('programa')
+export class ProgramaController {
+  constructor(private programaService: ProgramaService) {}
+
+  @Get()
+  async getAll(@Request() req: any, @Response() response: any) {
+    try {
+      const page = Number(req.query.page) || 1;
+      const pageSize = Number(req.query.pageSize) || 10;
+
+      const data = await this.programaService.getAll(page, pageSize);
+      responseSuccess(response, data);
+    } catch (error) {
+      responseError(response);
+    }
+  }
+
+  @Get(':search')
+  async search(@Param('search') search: string, @Response() response: any) {
+    try {
+      const data = await this.programaService.search(search);
+      responseSuccess(response, data);
+    } catch (error) {
+      responseError(response);
+    }
+  }
+
+  @Post()
+  async create(@Body() body: any, @Response() response: any) {
+    try {
+      const data = await this.programaService.create(body);
+      responseSuccess(response, data);
+    } catch (error) {
+      responseError(response);
+    }
+  }
+
+  @Put()
+  async put(@Body() body: any, @Response() response: any) {
+    try {
+      const data = await this.programaService.update(body);
+      responseSuccess(response, data);
+    } catch (error) {
+      responseError(response);
+    }
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number, @Response() response: any) {
+    try {
+      console.log(id);
+
+      const data = await this.programaService.delete(id);
+      responseSuccess(response, data);
+    } catch (error) {
+      responseError(response);
+    }
+  }
+}
---
+++ src/programa/programa.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { PrismaService } from 'src/prisma.service';
+import { ProgramaService } from './programa.service';
+import { ProgramaController } from './programa.controller';
+
+@Module({
+  providers: [ProgramaService, PrismaService],
+  exports: [ProgramaService],
+  controllers: [ProgramaController],
+})
+export class ProgramaModule {}
